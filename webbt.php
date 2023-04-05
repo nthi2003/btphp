f2fef48d35946af27aec89f46c9ee9dc3995b2d8
Lawll.n <92900098+nthi2003@users.noreply.github.com>
2023-04-05 21:43:54 +0700

    Add files via upload

--- webbt.php
+++ webbt.php
@@ -6,15 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> 
 </head>
 <body>
     <center>
+        <div class="main">
         <form action="">  
+            <div class="style-box">
             <label for="">Name: </label>
-            <input  class="name-box" type="text"><br>
-            <label for="">Price:</label>
+            <input  class="name-box" placeholder="Name" type="text">
+            </div>
+           <div class="style-box">
+           <label for="">Price:</label>
+            <input class="name-box" placeholder="Name" type="text" id="" name="">
+           </div>
+           <div class="style-box">
+           <label for="">Image:</label>
+            <input class="name-box" placeholder="Name" type="text" id="" name="">
+           </div>
+            <button class="box-upload">Upload</button>
          </form>
        
+        </div>
     </center>
 </body>
 </html>
